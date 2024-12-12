e902fa84371c07eb933401fcbf7e31836d28b269
Meteatakan <prometeatakan@gmail.com>
2024-12-12 08:01:45 +0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -11,12 +11,12 @@ router.post('/', async (req, res) => {
     const { password } = req.body;
 
 
-    if (password != PASSWORD) {
+    if (password. PASSWORD) {
         res.status(401).json({ message: 'Şifreniz hatalı' });
         return;
      }
      
-     res.status(200);
+     res.status(200).json({message:"Giriş Başarılı"});
    } catch (error) {
     console.log(error);
      res.status(500).json({ error: 'Barkod eklenemedi.' });
